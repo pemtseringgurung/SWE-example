72ec4e91b0c21e32a72a71610abc8429c6302181
Atomu26 <122555684+Atomu26@users.noreply.github.com>
2024-09-02 12:49:00 -0400

    Update PemWeek2Lab1.py

--- PemWeek2Lab1.py
+++ PemWeek2Lab1.py
@@ -2,7 +2,13 @@ import unittest
 
 class Parent:
     def multiply(self, x):
-        return x * 2
+        return x * 2 
+
+    def division(self, x):
+        return x / 2 
+
+    def addition(self, x):
+        return x + 10
 
 class Child(Parent):
     pass
@@ -17,8 +23,18 @@ class TestMultiply(unittest.TestCase):
         child_instance_second = Child()
         self.assertEqual(child_instance_second.multiply(8), 16)  
 
+    def test_division(self):
+        child_instance_third = Child()
+        self.assertEqual(child_instance_third.division(12),6)
+
+
+    def test_addtion(self):
+        child_instance_fifth = Child()
+        self.assertEqual(child_instance_fifth.addition(1),11)
+
+    def test_addition_again(self):
+        child_instance_sixth = Child()
+        self.assertEqual(child_instance_sixth.addition(10),20)
+
 if __name__ == '__main__':
     unittest.main()
-
-
-        
